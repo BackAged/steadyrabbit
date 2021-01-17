fed105330ccf9bbd69671471feaf81e86c510821
Shahin Mahmud <brotherapes@gmail.com>
2021-01-17 19:33:08 +0600

    fix format

--- error.go
+++ error.go
@@ -5,5 +5,5 @@ import "errors"
 // errors
 var (
 	ErrConnectionClosed = errors.New("connection has been closed")
-	ErrConnectionDial     = errors.New("connection dial error")
+	ErrConnectionDial   = errors.New("connection dial error")
 )
